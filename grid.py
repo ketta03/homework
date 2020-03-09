3c869ccd2cc223dcf6f1563642e8c9833d59cf4c
ketta03 <61695642+ketta03@users.noreply.github.com>
2020-03-09 12:27:52 +0300

    second homework file

---
+++ grid.py
@@ -0,0 +1,22 @@
+block_1 = "+ - - - - +"
+block_2 = "- - - - +"
+block_3 = "|         |         |"
+
+print(f"{block_1} {block_2}")
+print(block_3)
+print(block_3)
+print(block_3)
+print(block_3)
+print(block_3)
+print(block_3)
+print(block_3)
+print(f"{block_1} {block_2}")
+print(block_3)
+print(block_3)
+print(block_3)
+print(block_3)
+print(block_3)
+print(block_3)
+print(block_3)
+print(f"{block_1} {block_2}")
+
